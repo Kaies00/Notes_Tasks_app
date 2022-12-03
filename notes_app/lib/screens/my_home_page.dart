baa4a31894da2a968ec43f23095c9deb14e68ee3
Kaies00 <48424836+Kaies00@users.noreply.github.com>
2022-12-04 00:02:02 +0100

    more updates

--- notes_app/lib/screens/my_home_page.dart
+++ notes_app/lib/screens/my_home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
