81ec3a23d476c0dc989c39d2b7ed44866fbc1eba
Kaies00 <48424836+Kaies00@users.noreply.github.com>
2022-12-05 22:34:12 +0100

    add file code note

---
+++ notes_app/lib/sql_database/sql_db_1/pages/notes_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/values.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+import 'package:notes_app/values.dart';
+
+import '../db/notes_database.dart';
+import '../model/note.dart';
+import '../widegt/note_card_widget.dart';
+import 'edit_note_page.dart';
+import 'note_detail_page.dart';
+
+class NotesPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+
+    super.dispose();
+  }
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+
+    this.notes = await NotesDatabase.instance.readAllNotes();
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Notes',
+            style: const TextStyle(fontSize: 24),
+          ),
+          actions: [const Icon(Icons.search), const SizedBox(width: 12)],
+        ),
+        body: Center(
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : notes.isEmpty
+                  ? Column(
+                      children: [
+                        const Text(
+                          'Add Your First Note',
+                          style: TextStyle(color: Colors.purple, fontSize: 24),
+                        ),
+                      ],
+                    )
+                  : buildNotes(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          child: const Icon(Icons.add),
+          onPressed: () async {
+            await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const AddEditNotePage()),
+            );
+
+            refreshNotes();
+          },
+        ),
+      );
+
+  Widget buildNotes() => StaggeredGridView.countBuilder(
+        padding: const EdgeInsets.all(8),
+        itemCount: notes.length,
+        staggeredTileBuilder: (index) => const StaggeredTile.fit(2),
+        crossAxisCount: 4,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemBuilder: (context, index) {
+          final note = notes[index];
+
+          return GestureDetector(
+            onTap: () async {
+              await Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => NoteDetailPage(noteId: note.id!),
+              ));
+              refreshNotes();
+            },
+            child: NoteCardWidget(note: note, index: index),
+          );
+        },
+      );
+}
---
+++ notes_app/lib/values.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color aColor = Color(0xffF6D5D6);
+const Color bColor = Color(0xffDACAE5);
+const Color cColor = Color(0xffAEC4EB);
+const Color dColor = Color(0xffFFFDE0);
+const Color eColor = Color(0xff95CAF6);
+const Color fColor = Color(0xff3B4F8C);
+const Color accentPinkColor = Color(0xffFDE1EF);
+Color? backgroundColor = const Color(0xffEE9DC4);
+const Color pinkColor = Color(0xffEE9DC4);
+Color? plumColor = const Color(0xff701B71);
